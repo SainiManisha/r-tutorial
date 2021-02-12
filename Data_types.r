1382a989ef628cabb2fd8a687a03b8b5a5c50531
Manisha Saini <manisha.saini44@gmail.com>
2021-02-12 22:43:28 +0000

    add data types in R

---
+++ Data_types.r
@@ -0,0 +1,62 @@
+source('paste.r')
+source('paste.r')
+a<-"manisha"
+b<-"saini"
+paste(a,b)
+source('paste.r')
+source('paste.r')
+q()
+5
+5.5
+-5.5
+5/3
+class(5)
+class(5.5)
+class(-5.5)
+class(5/3)
+# logical data type in R
+class(TRUE)
+class(FALSE)
+a<-8; b<-9
+a>b
+class(a>b)
+b>a
+class(b>a)
+# character data type in R
+'A'
+"A"
+"manisha"
+"@"
+class('A')
+class("A")
+class("manisha")
+class("@")
+a<- "A"
+class(a)
+# complex data type in R
+1+2i
+4-5i
+class(1+2i)
+class(4-5i)
+# interger data type in R
+class(2L)
+class("2L")
+a<-as.integer(2)
+class(a)
+a<-2
+class(2)
+# data type conversions in R
+a<-6
+class(a)
+a1<-as.character(a)
+a1
+class(a1)
+# conversion of character data type to  logical data type
+a2<-as.logical(a1)
+class(a2)
+# conversion of logical data type to complex data type
+a3<-as.complex(a2)
+a3
+class(a3)
+
+
