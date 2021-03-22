c7660590b2bbe2f6e2086b3dc801ba2a7e38624a
Manisha Saini <manisha.saini44@gmail.com>
2021-03-22 07:10:01 +0000

    Add markovchain

---
+++ markovchain.r
@@ -0,0 +1,24 @@
+install.packages("markovchain")
+# install.packages("igraph")
+library(markovchain)
+
+states<-c("rainy","cloudy","sunny")
+probability_matrix<-rbind(c(0.0,0.4,0.6),c(0.4,0.1,0.5),c(0.6,0.2,0.2))
+rownames(probability_matrix)<-colnames(probability_matrix)<-states
+print(probability_matrix)
+
+mc <- new("markovchain", states = states, transitionMatrix = probability_matrix, name = "Weather")
+print(transitionProbability(mc, 'rainy', 'sunny'))
+
+show(mc)
+
+print('After 25 trials')
+print(mc ^ 25)
+
+library(igraph)
+png(filename="markovchain_graph.png")
+plot(mc)
+dev.off()
+
+initialState = c(1,0,0)
+print(initialState * mc ^ 25)
